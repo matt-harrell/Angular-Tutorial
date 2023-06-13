2d81ac0d6fa29abc3ca8a03c99ba87e542f2a802
Matt Harrell <matthew.629@hotmail.com>
2023-06-12 21:07:42 -0400

    complete lesson 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,16 @@ import { RouterModule } from '@angular/router';
   ],
   template: `
   <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
     <section class="content">
       <router-outlet></router-outlet>
     </section>
   </main>
-  `,
+`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
